f4df202aff265e11dd7fcd91cb4a811e8b283d0b
zimlit <zimlit2010@gmail.com>
2022-01-23 14:07:36 +0000

    fix license disclaimers

--- premake5.lua
+++ premake5.lua
@@ -1,6 +1,8 @@
 --[[
         Copyright 2022  Devin Rockwell
 
+    This file is part of Mango.
+
     Mango is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
